e333ea6e680256126908cf92f026c5f819cce3e2
abdulrahman <aabbcc2000107@gmail.com>
2021-09-25 22:56:51 +0300

    add commints to the code

--- python_pass.py
+++ python_pass.py
@@ -11,22 +11,23 @@ Instructions:
 
 class StringOperations:
     def reverse(self, to_be_reversed):
-
-        string_leangh = len(to_be_reversed)
-
-        
-
+        #a copy from input named reversed_string
         reversed_string  =  to_be_reversed
 
+        #reversing the copy by create a slice that starts with the length of the string, and ends at index 0
         reversed_string = reversed_string[::-1]
+        
+        #return a reversed copy
         return reversed_string
 
             
             
-
+#create a ReversedString class inherting from String operation class
 class ReversedString(StringOperations):
     pass
 
+#creat an Instantiate from ReversedString class
 string_to_revers = ReversedString()
 
+#print the output of the reverse function
 print(string_to_revers.reverse("hello world "))
