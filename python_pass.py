a29835e334b04e8d5b87692e6148e1bf0bfb44cb
Selfishmachine <91218301+Selfishmachine@users.noreply.github.com>
2021-09-25 23:13:13 +0300

    Update python_pass.py

--- python_pass.py
+++ python_pass.py
@@ -12,3 +12,9 @@ Instructions:
 class StringOperations:
     def reverse(self, *, to_be_reversed: str = None):
         raise NotImplemented('This method need to be implemented')
+
+class ReversedString(StringOperations):
+    def reverse(self, *, to_be_reversed: str = None): 
+        return to_be_reversed[::-1]
+rev = ReversedString()
+print(rev.reverse(to_be_reversed='Yanhad'))
