9788462af0ea9e28d87d4d29da43f3490670b76d
AbuAlhassan1 <qweqaz157@gmail.com>
2021-09-26 01:27:45 +0300

    done :)

--- python_pass.py
+++ python_pass.py
@@ -12,3 +12,14 @@ Instructions:
 class StringOperations:
     def reverse(self, *, to_be_reversed: str = None):
         raise NotImplemented('This method need to be implemented')
+
+class ReversedString(StringOperations):
+    def reverse(self, *, to_be_reversed: str = None): 
+        return to_be_reversed[::-1]
+
+def main():
+    ob = ReversedString();
+    print(ob.reverse(to_be_reversed="nassahlAubA"))
+
+if __name__ == "__main__":
+    main()
