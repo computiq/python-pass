29aeb8e53984b349170508d1c4f2ca1c682570ac
Baraa Basim <barabasim77@gmail.com>
2021-09-26 04:20:48 +0300

    override original function

--- python_pass.py
+++ python_pass.py
@@ -1,6 +1,5 @@
 """
 Instructions:
-
 1. Create a class named ReversedString that inherits from StringOperations class
 2. Implement the function reverse
 3. reverse function should be a one liner function that returns the reverse string to_be_reversed
@@ -10,15 +9,13 @@ Instructions:
 
 
 class StringOperations:
-    def reverse(self, *, to_be_reversed: str = "Hello World"):
-          return to_be_reversed[::-1]
+    def reverse(self, *, to_be_reversed: str = None):
+        raise NotImplemented('This method need to be implemented')
 
 class ReversedString(StringOperations):
-    pass
+    def reverse(self, *, to_be_reversed:str = "Hello World"):
+        return to_be_reversed[::-1]
+        
 
-
-      
-      
 ob = ReversedString()
-
 print(ob.reverse())
