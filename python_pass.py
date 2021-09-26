ace97b92215e0ca287486512018136a060984c5d
Abdullah <90783453+3b00dy@users.noreply.github.com>
2021-09-26 15:31:34 +0300

    Update python_pass.py

--- python_pass.py
+++ python_pass.py
@@ -10,5 +10,12 @@ Instructions:
 
 
 class StringOperations:
-    def reverse(self, *, to_be_reversed: str = None):
-        raise NotImplemented('This method need to be implemented')
+    def reverse(self, *, to_be_reversed: str ):return to_be_reversed[::-1]
+       
+class ReversedString (StringOperations):
+    def __init__(self):
+        super().reverse()
+
+message=ReversedString.reverse(self=None,to_be_reversed="esrever si siht nohtyP olleH")
+#the output= Hello Python this is reverse
+print(message)
