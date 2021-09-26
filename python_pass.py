6b0c8636430d24d743634535d9fda789b04947ff
unknown <ahmed.alyasry.gh@gmail.com>
2021-09-26 04:12:13 +0300

    The task has resolved as referred in instructions

--- python_pass.py
+++ python_pass.py
@@ -12,3 +12,12 @@ Instructions:
 class StringOperations:
     def reverse(self, *, to_be_reversed: str = None):
         raise NotImplemented('This method need to be implemented')
+
+
+class ReversedString(StringOperations):
+
+    def reverse(self,*, to_be_reversed: str = None):
+        return to_be_reversed[:: -1]
+
+word= ReversedString()
+print(word.reverse(to_be_reversed='YANHAD'))
