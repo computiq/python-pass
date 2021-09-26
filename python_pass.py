d35691af68d7423d3281e381621a397daa481af4
Hussain Alyasiry <72931678+xbanker3@users.noreply.github.com>
2021-09-26 12:45:49 +0300

    Update python_pass.py

--- python_pass.py
+++ python_pass.py
@@ -1,15 +1,10 @@
 class StringOperations:
     def reverse(self, *, to_be_reversed: str = None):
         raise NotImplemented('This method need to be implemented')
-    def __init__(self, to_be_reversed):
-        self.str = to_be_reversed
 
-    def reverse(self):
-        return self.str[::-1]
+class ReversedString (StringOperations):
+    def reverse(self, *, to_be_reversed: str = None):
+        return to_be_reversed[::-1]
 
-class ReversedString(StringOperations):
-    pass
-
-if __name__ == "__main__":
-    reversedname = ReversedString("hussain alyasiry")
-    print(reversedname.reverse())
+rev = ReversedString()
+print(rev.reverse(to_be_reversed=input("enter some text: ")))
