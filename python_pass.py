473833024994d3d2c2beb2c04f6c634a739f765a
kadhim <71837912+kadhim23py@users.noreply.github.com>
2021-09-26 16:25:20 +0300

    the correct answer to the task

--- python_pass.py
+++ python_pass.py
@@ -12,3 +12,12 @@ Instructions:
 class StringOperations:
     def reverse(self, *, to_be_reversed: str = None):
         raise NotImplemented('This method need to be implemented')
+class StringOperations:
+    def reverse(self, *, to_be_reversed: str = None):
+        raise NotImplemented('This method need to be implemented')
+
+class ReversedString(StringOperations):
+    def reverse(self, *, to_be_reversed: str = None): 
+        return to_be_reversed[::-1]
+revfun = ReversedString()
+print(revfun.reverse(to_be_reversed='kadhim'))
